e138aaf6a6879843297cb113c4e182a6292bb63b
Naveen M <117974950+naveen-m0804@users.noreply.github.com>
2025-12-30 14:11:15 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -3,7 +3,7 @@ import os
 FIREBASE_CRED_PATH = os.getenv("FIREBASE_CRED_PATH", "firebase_service.json")
 
 FIREBASE_DB_URL = (
-    "https://smart-dam-automation-system-default-rtdb.asia-southeast1.firebasedatabase.app/"
+    "https://********************.firebasedatabase.app/"
 )
 
 MODEL_PATH = os.getenv("MODEL_PATH", "rainfall_model.pkl")
